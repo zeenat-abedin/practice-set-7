cf7cac7309ecc6e623b0810fa47fd8d489463f29
zeenat <zeenu.23@gmail.com>
2024-03-26 13:14:42 +0530

    "Added Password class with generatePassword method to create a password with at least one lowercase, one uppercase and one special character"

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 // Create a JS program capable of generating a password which contains atleast one lowercase, one upperccase and one special character. Create a password class to achieve the same.
 
+class Password {
+    constructor() {
+        console.log("Welcome to Password Generator")
+        this.pass = ""
+    }
+    generatePassword(len) {
+        let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let numbers = "0123456789"
+        let special = "!@#$%^&*()_+~`|}{[]:;?><,./-="
+        if (len < 3) {
+            console.log("Your password should be atleast 3 characters long");
+        } else {
+            let i = 0
+            while (i < len) {
+                this.pass += chars[Math.floor(Math.random() * chars.length)];
+                this.pass += numbers[Math.floor(Math.random() * chars.length)];
+                this.pass += special[Math.floor(Math.random() * chars.length)];
+                i+=3
+            }
+            this.pass = this.pass.substring(0, len);
+            return this.pass
+        }
+    }
+}
+
+let p = new Password();
+console.log(p.generatePassword(7));
